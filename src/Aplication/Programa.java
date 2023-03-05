2b3a34cd940ec3f2cc826d958482d6cef99c2d5d
Igor Wolf <igor.wolf99@gmail.com>
2023-03-05 02:46:19 -0300

    Interação com usuário

--- src/Aplication/Programa.java
+++ src/Aplication/Programa.java
@@ -1,22 +1,36 @@
 package Aplication;
 
+import java.util.Scanner;
+
 import entities.Funcionario;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Funcionario func = new Funcionario("Sergio", "Melo", 34, 'M');
+		Scanner sc = new Scanner(System.in);
+
+// Cadastro Usuário
+		System.out.print("Nome: ");
+		String nome = sc.next();
+		System.out.print("Sobrenome: ");
+		String sobrenome = sc.next();
+		System.out.print("Idade: ");
+		int idade = sc.nextInt();
+		System.out.print("Sexo: ");
+		char sexo = sc.next().toUpperCase().charAt(0);
 		
+		Funcionario func = new Funcionario(nome, sobrenome, idade, sexo);
 		
-		
-		System.out.println("Setores"
+		System.out.println("\nSetores"
 				+ "\n1 - Recursos Humanos"
 				+ "\n2 - TI"
 				+ "\n3 - Compras"
 				+ "\n4 - Vendas"
 				+ "\n5 - Almoxarifado");
 		
-		func.setSetor(3);
+		System.out.print("Defina o Setor: ");
+		int set = sc.nextInt();
+		func.setSetor(set);
 		
 		
 		
